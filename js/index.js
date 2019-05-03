ad576e09ff6c8dd7137da2389e6aef96925e7bb1
Hendrik <mail@hendrikwallbaum.de>
2019-05-03 13:44:38 +0200

    :sparkles: Skin pop-up with loading animation.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const start = async (selector) => {
   const champions = await fetch('/testData/skins.json')
     .then(response => response.json())
-  console.log(champions)
 
   const championsForDisplay = await Promise.all(champions.map(champion => new Promise((resolve, reject) => {
     const image = new Image()
@@ -14,8 +13,6 @@ const start = async (selector) => {
     image.src = champion.squareImageUrl
   })))
 
-  console.log(championsForDisplay)
-
   const wrapper = document.querySelector(selector)
 
   championsForDisplay.forEach(champion => {
@@ -25,12 +22,63 @@ const start = async (selector) => {
     chaDiv.innerHTML += `<span>${champion.name}</span>`
     wrapper.appendChild(chaDiv)
 
-    chaDiv.addEventListener('click', () => displayChampion(champion))
+    chaDiv.addEventListener('click', (e) => displayChampion(e, champion))
   })
 }
 
-const displayChampion = champion => {
+const displayChampion = (event, champion) => {
   console.log(champion)
+  const skinPopUp = document.createElement('div')
+  skinPopUp.setAttribute('class', 'skin-pop-up')
+
+  document.body.appendChild(skinPopUp)
+  const { top: targetTop, left: targetLeft } = skinPopUp.getClientRects()[0]
+  const targetWidth = window.innerWidth * 0.7
+  const targetHeight = targetWidth * 6 / 11
+  const targetPosition = {
+    top: targetTop + 'px',
+    left: targetLeft + 'px',
+    height: targetHeight + 'px',
+    width: targetWidth + 'px'
+  }
+
+  // Get position and dimension of clicked element.
+  const clickedElement = event.target
+
+  // Bottom is bottom of element from top and right is right from left.
+  const { left: clickedLeft, top: clickedTop, right: clickedRight, bottom: clickedBottom } = clickedElement.getBoundingClientRect()
+  const clickedWidth = (clickedRight - clickedLeft)
+  const clickedHight = (clickedBottom - clickedTop)
+  const startPosition = {
+    left: clickedLeft + 'px',
+    top: clickedTop + 'px',
+    width: clickedWidth + 'px',
+    height: clickedHight + 'px'
+  }
+
+  // Position overlay where clicked element is.
+  // Make sure to use Object.assign here, spread doesn't work.
+  Object.assign(skinPopUp.style, startPosition)
+
+  // Add wrapped champion Icon with loader to pop-up.
+  const championImage = champion.image.cloneNode()
+  championImage.setAttribute('class', 'skin-pop-up__champion-image')
+  const championImageWrapper = document.createElement('div')
+  championImageWrapper.setAttribute('class', 'skin-pop-up__image-wrapper')
+  championImageWrapper.innerHTML = `
+  <div class="loader">
+    <svg class="circular" viewBox="25 25 50 50">
+      <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+    </svg>
+  </div>`
+  championImageWrapper.appendChild(championImage)
+  skinPopUp.appendChild(championImageWrapper)
+
+  skinPopUp.animate([startPosition, targetPosition], {
+    duration: 300,
+    fill: 'forwards',
+    easing: 'ease-out'
+  })
 }
 
 export default start
