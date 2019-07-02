e5b437efad0b23aad36316f4d650c1180fdbc638
Hendrik <mail@hendrikwallbaum.de>
2019-07-02 10:15:57 +0200

    :sparkles: add loading spinner to modal.

--- src/champions/skin-modal.tsx
+++ src/champions/skin-modal.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
 import { Skin } from './champion'
+import Loader from '../loader'
 
 const SkinModal = ({
   loadingImageURL,
@@ -9,8 +10,46 @@ const SkinModal = ({
   loadingImageURL: string
   skins: Skin[]
 }) => (
-  <div>
-    <img src={loadingImageURL} />
+  <div
+    css={css`
+      position: fixed;
+      left: 15vw;
+      top: 10vh;
+      width: 70vw;
+      height: 60vh;
+      background-color: #fff;
+      min-height: 20px;
+      animation-fill-mode: forwards;
+      box-shadow: 0 12px 15px 0 rgba(0, 0, 0, 0.24);
+    `}
+  >
+    <div
+      css={css`
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      `}
+    >
+      <img
+        src={loadingImageURL}
+        css={css`
+          border: 3px solid #1469cc;
+          border-radius: 50%;
+          box-sizing: border-box;
+          width: 84px;
+          height: 84px;
+        `}
+      />
+      <Loader
+        styles={css`
+          position: absolute;
+          top: -9px;
+          left: -9px;
+          width: 102px;
+        `}
+      />
+    </div>
   </div>
 )
 
---
+++ src/loader.tsx
@@ -0,0 +1,70 @@
+/** @jsx jsx */
+import { jsx, css, keyframes, SerializedStyles } from '@emotion/core'
+
+const rotate = keyframes`
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+const dash = keyframes`
+  0% {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 89, 200;
+    stroke-dashoffset: -35px;
+  }
+  100% {
+    stroke-dasharray: 89, 200;
+    stroke-dashoffset: -124px;
+  }
+`
+
+const Loader = ({ styles = css`` }: { styles?: SerializedStyles }) => (
+  <div
+    css={css`
+      ${styles}
+      &:before {
+        content: '';
+        display: block;
+        padding-top: 100%;
+      }
+    `}
+  >
+    <svg
+      css={css`
+        animation: ${rotate} 2s linear infinite;
+        height: 100%;
+        transform-origin: center center;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+      `}
+      viewBox="25 25 50 50"
+    >
+      <circle
+        css={css`
+          stroke-dasharray: 1, 200;
+          stroke-dashoffset: 0;
+          animation: ${dash} 1.5s ease-in-out infinite;
+          stroke: #ffd700;
+          stroke-linecap: round;
+        `}
+        cx="50"
+        cy="50"
+        r="20"
+        fill="none"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+    </svg>
+  </div>
+)
+
+export default Loader
