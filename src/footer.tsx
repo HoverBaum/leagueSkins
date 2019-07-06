7c216c5177e0c3ed83d255145c5847ce66ae4e00
Hendrik <mail@hendrikwallbaum.de>
2019-07-06 21:52:19 +0200

    :bug: fix footer divider.

--- src/footer.tsx
+++ src/footer.tsx
@@ -1,43 +1,35 @@
-import { Fragment } from 'react'
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
 import { Link } from 'react-router-dom'
 
 const Footer = () => (
-  <Fragment>
-    <div
+  <footer
+    css={css`
+      margin-top: 4rem;
+      padding: 2rem;
+      padding-bottom: 0.5rem;
+      display: flex;
+      justify-content: center;
+      border-top: 1px solid #02182b;
+    `}
+  >
+    <ul
       css={css`
-        height: 1px;
-        background-color: #02182b;
-        margin-top: 4rem;
-      `}
-    />
-    <footer
-      css={css`
-        padding: 2rem;
-        padding-bottom: 0.5rem;
         display: flex;
-        justify-content: center;
+        & li {
+          list-style-type: none;
+          margin-right: 1rem;
+        }
       `}
     >
-      <ul
-        css={css`
-          display: flex;
-          & li {
-            list-style-type: none;
-            margin-right: 1rem;
-          }
-        `}
-      >
-        <li>
-          <Link to="/">Skins</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </footer>
-  </Fragment>
+      <li>
+        <Link to="/">Skins</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  </footer>
 )
 
 export default Footer
