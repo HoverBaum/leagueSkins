ad860f363881846e728fdc1a65136a0aa2191c18
Hendrik <mail@hendrikwallbaum.de>
2019-07-06 22:00:42 +0200

    :sparkles: Add a backdrop for modal.

--- src/champions/skin-modal.tsx
+++ src/champions/skin-modal.tsx
@@ -3,7 +3,7 @@ import { jsx, css } from '@emotion/core'
 import { Skin, SkinLoaded } from './champion'
 import Loader from '../loader'
 import useOutsideClick from '../hooks/useOutsideClick'
-import { useRef, useEffect, useState } from 'react'
+import { useRef, useEffect, useState, Fragment } from 'react'
 import useWindowSize from '../hooks/useWindowSize'
 import ChampionSpinner from './champion-spinner'
 
@@ -70,52 +70,67 @@ const SkinModal = ({
   }, [skins])
 
   return (
-    <div
-      ref={ref}
-      css={css`
-        position: fixed;
-        left: 15vw;
-        top: 10vh;
-        width: ${width * 0.7}px;
-        height: ${(width * 0.7 * 9) / 16}px;
-        background-color: #fff;
-        min-height: 20px;
-        animation-fill-mode: forwards;
-        box-shadow: 0 12px 15px 0 rgba(0, 0, 0, 0.24);
-      `}
-    >
-      {loadedSkins.length === 0 && shouldDisplayLoaderYet && (
-        <div
-          css={css`
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-          `}
-        >
-          <img
-            src={loadingImageURL}
+    <Fragment>
+      <div
+        css={css`
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100vw;
+          height: 100vh;
+          background-color: white;
+          opacity: 0.666;
+          cursor: pointer;
+        `}
+      />
+      <div
+        ref={ref}
+        css={css`
+          position: fixed;
+          left: 15vw;
+          top: 10vh;
+          z-index: 10;
+          width: ${width * 0.7}px;
+          height: ${(width * 0.7 * 9) / 16}px;
+          background-color: #fff;
+          min-height: 20px;
+          animation-fill-mode: forwards;
+          box-shadow: 0 12px 15px 0 rgba(0, 0, 0, 0.24);
+        `}
+      >
+        {loadedSkins.length === 0 && shouldDisplayLoaderYet && (
+          <div
             css={css`
-              border: 3px solid #1469cc;
-              border-radius: 50%;
-              box-sizing: border-box;
-              width: 84px;
-              height: 84px;
-            `}
-            alt="Loading skins"
-          />
-          <Loader
-            styles={css`
               position: absolute;
-              top: -9px;
-              left: -9px;
-              width: 102px;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
             `}
-          />
-        </div>
-      )}
-      {loadedSkins.length > 0 && <ChampionSpinner skins={loadedSkins} />}
-    </div>
+          >
+            <img
+              src={loadingImageURL}
+              css={css`
+                border: 3px solid #1469cc;
+                border-radius: 50%;
+                box-sizing: border-box;
+                width: 84px;
+                height: 84px;
+              `}
+              alt="Loading skins"
+            />
+            <Loader
+              styles={css`
+                position: absolute;
+                top: -9px;
+                left: -9px;
+                width: 102px;
+              `}
+            />
+          </div>
+        )}
+        {loadedSkins.length > 0 && <ChampionSpinner skins={loadedSkins} />}
+      </div>
+    </Fragment>
   )
 }
 
